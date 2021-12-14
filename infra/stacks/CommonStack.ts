8b78b56a86124542f10c5b8439fc7ba513c8f144
awmaker <awmaker@kakao.com>
2021-12-14 22:42:46 +0900

    ENH: update cdk

--- infra/stacks/CommonStack.ts
+++ infra/stacks/CommonStack.ts
@@ -1,21 +1,14 @@
 import { App, Stack, StackProps } from '@aws-cdk/core'
-import { AllowVpcPeeringDnsResolution } from '../modules/allowVpcPeeringDnsResolution'
-import { DomainName, EndpointType, RestApi } from '@aws-cdk/aws-apigateway'
-import { RecordTarget } from '@aws-cdk/aws-route53'
-import { ApiGatewayDomain } from '@aws-cdk/aws-route53-targets'
-import { Peer, Port } from '@aws-cdk/aws-ec2'
-
-import setting from '../setting'
-import hostedZoneModule from '../modules/hostedZone'
-import certificateModule from '../modules/certificate'
-import lambdaRestApiModule from '../modules/lambdaRestApi'
-import vpcModule from '../modules/vpc'
-import vpcPeeringModule from './../modules/vpcPeering'
-import lambdaModule from '../modules/lambda'
-import route from '../modules/route'
+import {
+    Cors,
+    EndpointType,
+    MethodLoggingLevel,
+    RestApi,
+} from '@aws-cdk/aws-apigateway'
 import convertSwaggerToCdkRestApiModule from '../modules/convertSwaggerToCdkRestApi'
-import aRecordModule from '../modules/aRecord'
-import securityGroupModule from './../modules/securityGroup'
+import { Runtime } from '@aws-cdk/aws-lambda'
+import { RetentionDays } from '@aws-cdk/aws-logs'
+import { SubnetType } from '@aws-cdk/aws-ec2'
 
 interface props extends StackProps {
     swagger: any
@@ -24,113 +17,41 @@ class CommonStack extends Stack {
     public constructor(scope: App, key: string, props?: props) {
         super(scope, key, props)
 
-        const hostedZone = hostedZoneModule(
-            this,
-            setting.hostedZone.cdkHostedZone.key,
-            {
-                ...setting.hostedZone.cdkHostedZone,
-            }
-        )
-        const certificate = certificateModule(
-            this,
-            setting.certificate.cdkCertificate.key,
-            {
-                ...setting.certificate.cdkCertificate,
-                domainName: `${setting.certificate.cdkCertificate.subDomain}.${setting.hostedZone.cdkHostedZone.zoneName}`,
-                hostedZone,
-            }
-        )
-        const vpc = vpcModule(this, setting.vpc.cdkVpc.key, {
-            ...setting.vpc.cdkVpc,
+        const slipyApiGateway = new RestApi(this, 'slipyCdkApiGateway', {
+            restApiName: 'Slipy_API_Gataway',
+            description: 'slipy api gateways',
+            endpointTypes: [EndpointType.REGIONAL],
+            failOnWarnings: true, // Rollback when error on deploy process
+            defaultCorsPreflightOptions: {
+                allowOrigins: Cors.ALL_ORIGINS,
+                allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTION'],
+            },
+            deployOptions: {
+                stageName: process.env.INFRA_ENV,
+                description: `stage in ${process.env.INFRA_ENV} environment`,
+                variables: {
+                    APP_ENV: process.env.INFRA_ENV,
+                },
+                tracingEnabled: true, // X-Ray enable
+                dataTraceEnabled: true, // CloudWatch log enable
+                loggingLevel: MethodLoggingLevel.INFO, // CloudWatch logging level
+                throttlingBurstLimit: 10, // maximum process count at same time
+                throttlingRateLimit: 10, // maximum request count per second
+            },
         })
-        const vpcPeering = vpcPeeringModule(
-            this,
-            setting.vpc.cdkVpc.peering.key,
-            {
-                ...setting.vpc.cdkVpc.peering,
-                vpcId: vpc.vpcId,
-            }
-        )
-        new AllowVpcPeeringDnsResolution(
-            this,
-            setting.vpc.cdkVpc.peering.dnsKey,
-            {
-                vpcPeering,
-            }
-        )
-        vpc.publicSubnets.forEach(({ routeTable: { routeTableId } }, idx) => {
-            route(this, `${setting.vpc.cdkVpc.routeTable.key}_${idx}`, {
-                destinationCidrBlock:
-                    setting.vpc.cdkVpc.peering.destinationCidrBlock,
-                routeTableId,
-                vpcPeeringConnectionId: vpcPeering.ref,
-            })
-        })
-        const securityGroup = securityGroupModule(
-            this,
-            setting.vpc.cdkVpc.securityGroup.key,
-            { ...setting.vpc.cdkVpc.securityGroup, vpc }
-        )
-        securityGroup.addIngressRule(
-            Peer.ipv4(setting.vpc.cdkVpc.peering.destinationCidrBlock),
-            Port.allTcp(),
-            'allow all access from the DB'
-        )
-        // const handler = lambdaModule(this, setting.lambda.cdkHandler.key, {
-        //     ...setting.lambda.cdkHandler,
-        //     vpc,
-        // })
-        // const apiGateway = lambdaRestApiModule(
-        //     this,
-        //     setting.apiGateway.cdkApiGateway.key,
-        //     {
-        //         ...setting.apiGateway.cdkApiGateway,
-        //         handler,
-        //     }
-        // )
-        // const aRecord = aRecordModule(
-        //     this,
-        //     setting.route53.aRecord.cdkARecord.key,
-        //     {
-        //         zone: hostedZone,
-        //         recordName: `${setting.certificate.cdkCertificate.subDomain}.${setting.hostedZone.cdkHostedZone.zoneName}`,
-        //         target: RecordTarget.fromAlias(
-        //             new ApiGatewayDomain(
-        //                 new DomainName(
-        //                     this,
-        //                     setting.apiGateway.cdkApiGateway.domain.key,
-        //                     {
-        //                         certificate,
-        //                         domainName: `${setting.certificate.cdkCertificate.subDomain}.${setting.hostedZone.cdkHostedZone.zoneName}`,
-        //                         endpointType: EndpointType.EDGE,
-        //                         mapping: apiGateway,
-        //                     }
-        //                 )
-        //             )
-        //         ),
-        //     }
-        // )
-        const testApiGateway = new RestApi(this, 'testCdkApiGateway', {
-            restApiName: 'My_Test_API',
-            ...setting.apiGateway.myTestApiGateway,
-        })
-        new ApiGatewayDomain(
-            new DomainName(
-                this,
-                setting.apiGateway.myTestApiGateway.domain.key,
-                {
-                    ...setting.apiGateway.myTestApiGateway.domain,
-                    certificate,
-                    domainName: `${setting.certificate.cdkCertificate.subDomain}.${setting.hostedZone.cdkHostedZone.zoneName}`,
-                    endpointType: EndpointType.EDGE,
-                    mapping: testApiGateway,
-                }
-            )
-        )
-        convertSwaggerToCdkRestApiModule(this, testApiGateway, props.swagger, {
-            ...setting.lambda.commonFunctions,
-            vpc,
-            securityGroups: [securityGroup],
+        convertSwaggerToCdkRestApiModule(this, slipyApiGateway, props.swagger, {
+            key: 'slipyFunctions',
+            runtime: Runtime.NODEJS_14_X,
+            allowPublicSubnet: true,
+            logRetention: RetentionDays.TEN_YEARS,
+            environment: {
+                APP_ENV: process.env.INFRA_ENV,
+            },
+            vpcSubnets: {
+                subnetType: SubnetType.PUBLIC,
+            },
+            // vpc,
+            // securityGroups: [securityGroup],
         })
     }
 }
