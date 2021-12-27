c814cd9ef15857b142a5a4eee6c1ed0456efb41e
sri02075 <sri02075@naver.com>
2021-12-28 00:46:23 +0900

    ENH: add bundle

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -28,6 +28,8 @@ const readDirectory = async (searchPath: any): Promise<void> => {
 	}
 }
 
+
+
 const bundle = async () => {
 	await fs.mkdir(tempPath, { recursive: true })
 	const commonModulePath = path.join(fromPath, './modules/common.ts')
@@ -43,7 +45,7 @@ const bundle = async () => {
 }
 
 bundle()
-
+// bundleLibrary()
 // 1. src 내부의 API들을 읽어서 APIs를 최상위구조에 할당. lambda 이름을 붙인 폴더 내부에 구조 복사
 // 2. modules는 최상위구조에 할당.
 // 3. 라이브러리는 일단 나중에 1, 2부터 ㄱㄱ
